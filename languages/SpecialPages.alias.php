5c3c7b2c50e64ebe6964b7afbc29e21e63d97ef7
hslater <hamish.slater@hallowelt.com>
2024-11-07 23:16:07 +1300

    Add SpecialPages.alias
    ERM20742
    
    [REL1_39, REL1_39-1.0.x, REL1_43, master]
    
    Change-Id: I505ed1eb501edeb2146ea180be76991fbdc23fa0

---
+++ languages/SpecialPages.alias.php
@@ -0,0 +1,15 @@
+<?php
+
+$specialPageAliases = [];
+
+/** English (English) */
+$specialPageAliases['en'] = [
+	'NotificationCenter' => [ 'NotificationCenter', 'Notification Center' ],
+	'NotificationTester' => [ 'NotificationTester', 'Notification Tester' ]
+];
+
+/** German (Deutsch) */
+$specialPageAliases['de'] = [
+	'NotificationCenter' => [ 'NotificationCenter', 'Notification Center' ],
+	'NotificationTester' => [ 'NotificationTester', 'Notification Tester' ]
+];
