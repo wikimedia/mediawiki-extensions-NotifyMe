ee8bae31e7aa65ad31a6b4f720b731cf2bd6f56a
hslater <hamish.slater@hallowelt.com>
2025-02-10 18:36:00 +0100

    Update HTMLFormField namespace
    ERM38444
    
    [REL1_43, master]

--- src/MediaWiki/Html/NotificationsSubscriptionsElement.php
+++ src/MediaWiki/Html/NotificationsSubscriptionsElement.php
@@ -2,8 +2,8 @@
 
 namespace MediaWiki\Extension\NotifyMe\MediaWiki\Html;
 
-use HTMLFormField;
 use MediaWiki\Html\Html;
+use MediaWiki\HTMLForm\HTMLFormField;
 use MWException;
 use OOUI\ProgressBarWidget;
 
