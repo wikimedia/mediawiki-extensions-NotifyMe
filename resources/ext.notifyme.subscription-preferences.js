5a76ded084acaf0236820c1fd35a9220c6a987c5
Mschlindwein <schlindwein@hallowelt.com>
2025-07-29 14:03:31 +0200

    Add notification panel to EnhancedPreferences
    ERM42837
    
    [5.x]
    
    Depends-On: I081f741523f82806e040b731b4b66ba7e2bd2d39
    
    Change-Id: I31a2f883557070807e4fe53c2c8eb3eed13a67c5
    (cherry picked from commit c85fc91a51a16d37710599f1cfe403272e1b0e0b)

--- resources/ext.notifyme.subscription-preferences.js
+++ resources/ext.notifyme.subscription-preferences.js
@@ -1,23 +1,44 @@
-$( function () {
+$( function ( mw ) {
 	const $form = $( '#mw-prefs-form' );
 	const $container = $form.find( '.notifications-subscriptions' );
 	const $input = $form.find( 'input.ext-notifyme-subscriptions-hidden' );
 
-	const manager = new ext.notifyme.ui.panel.SubscriptionManager( {
-		data: JSON.parse( $input.val() ),
-		channelConfigurationRegistry: ext.notifyme.channelConfigurationRegistry,
-		buckets: $container.data( 'buckets' ),
-		events: $container.data( 'events' ),
-		channelLabels: $container.data( 'channel-labels' )
-	} );
-	manager.connect( this, {
-		change: function ( value ) {
-			$input.val( JSON.stringify( value ) );
-			// Trigger change event so that the form is marked as dirty
-			const event = new Event( 'change' );
-			document.dispatchEvent( event );
+	if ( $input.length > 0 ) {
+		const manager = new ext.notifyme.ui.panel.SubscriptionManager( {
+			data: JSON.parse( $input.val() ),
+			channelConfigurationRegistry: ext.notifyme.channelConfigurationRegistry,
+			buckets: $container.data( 'buckets' ),
+			events: $container.data( 'events' ),
+			channelLabels: $container.data( 'channel-labels' )
+		} );
+		manager.connect( this, {
+			change: function ( value ) {
+				$input.val( JSON.stringify( value ) );
+				// Trigger change event so that the form is marked as dirty
+				const event = new Event( 'change' );
+				document.dispatchEvent( event );
+			}
+		} );
+		$container.find( '.oo-ui-progressBarWidget' ).remove();
+		$container.append( manager.$element );
+	}
+
+	mw.hook( 'enhanced-standard-preferences-setup-sections' ).add( ( context, prefs ) => {
+		const pref = prefs.notifications.subs[ 0 ];
+		if ( pref.type !== 'notifications-subscriptions' ) {
+			return;
 		}
+		pref.type = 'custom';
+		pref.content = {
+			classname: 'ext.notifyme.ui.panel.SubscriptionManager',
+			pref: 'notifications-subscriptions',
+			cfg: {
+				data: JSON.parse( pref.default ),
+				channelConfigurationRegistry: ext.notifyme.channelConfigurationRegistry,
+				buckets: pref.value.bucketData,
+				events: pref.value.eventData,
+				channelLabels: pref.value.channelLabels
+			}
+		};
 	} );
-	$container.find( '.oo-ui-progressBarWidget' ).remove();
-	$container.append( manager.$element );
-} );
+}( mediaWiki ) );
