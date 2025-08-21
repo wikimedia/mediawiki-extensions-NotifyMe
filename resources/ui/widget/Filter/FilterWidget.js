28dfa2569bb67339afa9b8ce71505b6964edcb50
Mschlindwein <schlindwein@hallowelt.com>
2025-08-21 11:57:05 +0200

    [WCAG] Improve notification filter widget
    ERM42965
    
    [5.x]
    
    Depends-On: Ice874816b272a6b0556ddaf749bde10559c2d214
    
    Change-Id: I3bf9c6d67e5b9da26289fab7b8535f196d23510e
    (cherry picked from commit eb5a6785ff01c0e11249c717b18c6a340e02ca6e)

--- resources/ui/widget/Filter/FilterWidget.js
+++ resources/ui/widget/Filter/FilterWidget.js
@@ -19,153 +19,39 @@ ext.notifyme.ui.widget.FilterWidget.prototype.loadData = function ( filterData, 
 };
 
 ext.notifyme.ui.widget.FilterWidget.prototype.updateContent = function () {
-	const $content = $( '<ul>' ).addClass( 'notifications-ui-widget-FilterWidget-filters-list' )
-		.attr( 'tabindex', 0 );
-
+	const options = [];
 	for ( const filter in this.filterData ) {
-		let $listItem;
-
 		const filterType = this.filterData[ filter ].type;
-
-		if ( !this.filterData[ filter ].hasOwnProperty( 'items' ) ) {
-			$listItem = this.generateListItem( this.filterData[ filter ], filterType );
-		} else {
-			$listItem = this.generateSublistTitle( this.filterData[ filter ] );
+		if ( filterType !== 'title' ) {
+			options.push( { optgroup: this.filterData[ filter ].label } );
 		}
-
-		$content.append( $listItem );
-
-		// If that's multiple level filter, with sub filters.
-		// For example
-		//      Single level filter:
-		//          Everything - 99
-		//      Multiple level filter:
-		//          Namespaces
-		//              Namespace1 - 8
-		//              Namespace2 - 6
-		if ( this.filterData[ filter ].hasOwnProperty( 'items' ) ) {
-			// Add sub filters
-
-			// As soon as we should not insert "<ul>" directly as child of "<ul>" (violates WCAG)
-			// Then at first wrap it into "<li>"
-			const $subListWrapper = $( '<li>' ).addClass( 'notifications-ui-widget-FilterWidget-sublist-wrapper' );
-			const $subList = $( '<ul>' ).addClass( 'notifications-ui-widget-FilterWidget-sublist' );
-
-			for ( const item in this.filterData[ filter ].items ) {
-				const $subListItem = this.generateListItem( this.filterData[ filter ].items[ item ], filterType );
-				$subListItem.addClass( 'notifications-ui-widget-FilterWidget-sublist-item' );
-
-				$subList.append( $subListItem );
-			}
-
-			$subListWrapper.append( $subList );
-
-			$content.append( $subListWrapper );
+		for ( const item in this.filterData[ filter ].items ) {
+			const filterKey = filterType + '-' + this.filterData[ filter ].items[ item ].key;
+			const count = this.filterData[ filter ].items[ item ].count;
+			options.push( {
+				data: filterKey,
+				label: this.filterData[ filter ].items[ item ].label,
+				batch: count,
+				batchText: mw.message( 'notifyme-notification-filter-count-aria-label', count ).text()
+			} );
 		}
 	}
-
-	$( document ).on( 'click', '.notifications-ui-widget-FilterWidget-item', ( e ) => {
-		const $target = $( e.target );
-
-		if ( $target.hasClass( 'notifications-ui-widget-FilterWidget-item' ) ) {
-			this.selectItem( $target );
-		} else {
-			const $actualItem = $target.closest( '.notifications-ui-widget-FilterWidget-item' );
-			if ( $actualItem.length ) {
-				this.selectItem( $actualItem );
-			}
-		}
-
-		e.stopPropagation();
-	} );
-
-	// Consider selecting necessary filter option using keyboard, with "Enter"
-	$( document ).on( 'keyup', '.notifications-ui-widget-FilterWidget-item', ( e ) => {
-		const $target = $( e.target );
-
-		let $actualItem;
-
-		if ( $target.hasClass( 'notifications-ui-widget-FilterWidget-item' ) ) {
-			$actualItem = $target;
-		} else {
-			$actualItem = $target.closest( '.notifications-ui-widget-FilterWidget-item' );
-			if ( $actualItem.length ) {
-				this.selectItem( $actualItem );
-			}
-		}
-
-		// Just to make sure
-		// But actually this condition should always evaluate
-		if ( document.activeElement === $actualItem[ 0 ] ) {
-			// "Enter"
-			if ( e.which === 13 ) {
-				this.selectItem( $actualItem );
-			}
-		}
-
-		e.stopPropagation();
-	} );
-
-	this.$element.empty().append( $content );
-};
-
-/**
- * @private
- * @param {Object} item
- * @param {string} filterType
- * @return {HTMLElement}
- */
-ext.notifyme.ui.widget.FilterWidget.prototype.generateListItem = function ( item, filterType ) {
-	const $listItem = $( '<li>' ).addClass( 'notifications-ui-widget-FilterWidget-item' )
-		.attr( 'tabindex', 0 )
-		.attr(
-			'aria-label',
-			mw.message( 'notifyme-notification-center-filter-item-aria-label', item.label ).text()
-		);
-
-	const $itemLabel = $( '<span>' )
-		.addClass( 'notifications-ui-widget-FilterWidget-item-label' )
-		.html( item.label );
-	const $itemCount = $( '<span>' )
-		.addClass( 'notifications-ui-widget-FilterWidget-item-count' )
-		.html( item.count );
-
-	$listItem.append(
-		$itemLabel, $itemCount
-	);
-
-	const filterKey = filterType + '-' + item.key;
-
-	if ( filterKey === this.activeFilter ) {
-		$listItem.addClass( 'notifications-ui-widget-FilterWidget-item-active' );
-
-		$listItem.attr( 'aria-current', 'true' );
+	if ( !this.selectWidget ) {
+		this.selectWidget = new OOJSPlus.ui.widget.GroupedSelectWidget( {
+			options: options
+		} );
+		this.selectWidget.getMenu().$element.attr( 'aria-label',
+			mw.message( 'notifyme-notification-filter-aria-label' ).text() );
+		this.selectWidget.connect( this, {
+			select: 'filterSelected'
+		} );
+		this.$element.append( this.selectWidget.$element );
 	}
-
-	$listItem.attr( 'data-key', filterKey );
-
-	return $listItem;
+	this.selectWidget.getMenu().selectItemByData( this.activeFilter );
 };
 
-/**
- * @private
- * @param {Object} item
- * @return {HTMLElement}
- */
-ext.notifyme.ui.widget.FilterWidget.prototype.generateSublistTitle = function ( item ) {
-	const $sublistTitle = $( '<li>' ).addClass( 'notifications-ui-widget-FilterWidget-sublist-title' );
+ext.notifyme.ui.widget.FilterWidget.prototype.filterSelected = function () {
+	const nsOption = this.selectWidget.getMenu().findSelectedItem();
 
-	const $itemLabel = $( '<span>' )
-		.addClass( 'notifications-ui-widget-FilterWidget-item-label' )
-		.html( item.label );
-
-	$sublistTitle.append( $itemLabel );
-
-	return $sublistTitle;
-};
-
-ext.notifyme.ui.widget.FilterWidget.prototype.selectItem = function ( $item ) {
-	const key = $item.attr( 'data-key' );
-
-	this.emit( 'selectItem', key );
+	this.emit( 'selectItem', nsOption.data );
 };
