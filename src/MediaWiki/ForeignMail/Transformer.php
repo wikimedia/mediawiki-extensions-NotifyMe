7e33fd8acbc10ad07b104f17e27d78b0a79c3091
hslater <hamish.slater@hallowelt.com>
2025-09-12 23:26:24 +0700

    Fix email whitespace handling
    Prevent adding spaces in usernames
    Add space after delimiters only before uppercase (new sentence)
    
    ERM43879
    
    [5.1.x]
    
    Change-Id: I1ed778fd630cfed5e11dc9e51eca8cfaf761a7fb

--- src/MediaWiki/ForeignMail/Transformer.php
+++ src/MediaWiki/ForeignMail/Transformer.php
@@ -2,9 +2,13 @@
 
 namespace MediaWiki\Extension\NotifyMe\MediaWiki\ForeignMail;
 
+use Exception;
+use MailAddress;
 use MediaWiki\Extension\NotifyMe\Channel\Email\MailContentProvider;
+use MediaWiki\Extension\NotifyMe\MediaWiki\ForeignMail\Processor\IProcessor;
 use MediaWiki\Hook\UserMailerTransformMessageHook;
 use MediaWiki\User\User;
+use Psr\Log\LoggerInterface;
 
 /**
  * TODO: This class lost a lot of its meaning since we dont use the "wrapper" anymore
@@ -14,14 +18,18 @@ class Transformer implements UserMailerTransformMessageHook {
 	 * @var MailContentProvider
 	 */
 	private $mailContentProvider;
-	/** @var array */
+	/** @var IProcessor[] */
 	private $processors;
+	/** @var LoggerInterface */
+	protected $logger;
 
 	/**
 	 * @param MailContentProvider $mailContentProvider
+	 * @param LoggerInterface $logger
 	 */
-	public function __construct( MailContentProvider $mailContentProvider ) {
+	public function __construct( MailContentProvider $mailContentProvider, LoggerInterface $logger ) {
 		$this->mailContentProvider = $mailContentProvider;
+		$this->logger = $logger;
 		// Don't think we need a registry for this, yet
 		$this->processors = [
 			new Processor\Links(),
@@ -54,17 +62,55 @@ class Transformer implements UserMailerTransformMessageHook {
 	 * @inheritDoc
 	 */
 	public function onUserMailerTransformMessage( $to, $from, &$subject, &$headers, &$body, &$error ) {
+		$this->logger->debug( 'Transformer::onUserMailerTransformMessage $body=' . $body );
+
 		if ( $this->isPlainText( $headers ) ) {
 			try {
+				$this->quotePeriodUsernames( $to, $body );
+				$body = $this->transformToHtml( $body );
+				$this->unquotePeriodUsernames( $to, $body );
+
 				$body = $this->mailContentProvider->wrap(
-					$this->transformToHtml( $body ),
+					$body,
 					User::newSystemUser( 'MediaWiki default' )
 				);
 
 				$headers['Content-type'] = 'text/html; charset=UTF-8';
-			} catch ( \Exception $e ) {
+			} catch ( Exception $e ) {
 				// Do nothing, we'll just send the plain text email
 			}
 		}
 	}
+
+	/**
+	 * Wrap usernames containing a period in quotes
+	 * Used to preserve usernames in LineBreak::fixWhitespaces
+	 *
+	 * @param MailAddress[] $to
+	 * @param string &$body
+	 * @return string
+	 */
+	private function quotePeriodUsernames( $to, string &$body ): void {
+		foreach ( $to as $t ) {
+			if ( str_contains( $t->name, '.' ) ) {
+				$body = str_replace( $t->name, "\"$t->name\"", $body );
+			}
+		}
+	}
+
+	/**
+	 * Restore quoted usernames containing a period back to original
+	 *
+	 * @param MailAddress[] $to
+	 * @param string &$body
+	 * @return string
+	 */
+	private function unquotePeriodUsernames( $to, string &$body ): void {
+		foreach ( $to as $t ) {
+			if ( str_contains( $t->name, '.' ) ) {
+				$body = str_replace( "\"$t->name\"", $t->name, $body );
+			}
+		}
+	}
+
 }
