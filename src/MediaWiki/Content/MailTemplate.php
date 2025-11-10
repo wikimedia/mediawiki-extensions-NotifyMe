686c94a47063ac296200e0e1247a04332fd73856
d.savuljesku <d.savuljesku@gmail.com>
2025-11-10 13:09:04 +0100

    Make HTML preview of mail pages work again
    Move rendering to ContentHandler instead of the Content itself
    
    [5.1.x]
    
    ERM38583
    
    Change-Id: If64ff7597a33490041f6f8603b01de3cc257f1ac
    (cherry picked from commit 871476bd069ecc838fc4418daaf4dfcd805c7cc9)

--- src/MediaWiki/Content/MailTemplate.php
+++ src/MediaWiki/Content/MailTemplate.php
@@ -2,28 +2,10 @@
 
 namespace MediaWiki\Extension\NotifyMe\MediaWiki\Content;
 
-use Article;
 use Exception;
 use MediaWiki\Content\TextContent;
-use MediaWiki\Context\RequestContext;
-use MediaWiki\Extension\NotifyMe\Channel\Email\MailContentProvider;
-use MediaWiki\Html\Html;
-use MediaWiki\MediaWikiServices;
-use MediaWiki\Message\Message;
-use MediaWiki\Parser\ParserOptions;
-use MediaWiki\Parser\ParserOutput;
-use MediaWiki\Revision\RevisionLookup;
-use MediaWiki\Title\Title;
 
 class MailTemplate extends TextContent {
-	/**
-	 * @var MailContentProvider
-	 */
-	private $mailContentProvider;
-	/**
-	 * @var RevisionLookup
-	 */
-	private $revisionLookup;
 
 	/**
 	 * @param string $text
@@ -32,101 +14,5 @@ class MailTemplate extends TextContent {
 	 */
 	public function __construct( $text ) {
 		parent::__construct( $text, 'mail_template' );
-		$this->revisionLookup = MediaWikiServices::getInstance()->getRevisionLookup();
-		$this->mailContentProvider = MediaWikiServices::getInstance()->getService(
-			'NotifyMe.MailContentProvider'
-		);
 	}
-
-	/**
-	 * @param Title $title
-	 * @param int $revId
-	 * @param ParserOptions $options
-	 * @param bool $generateHtml
-	 * @param ParserOutput &$output
-	 *
-	 * @return void
-	 */
-	protected function fillParserOutput(
-		Title $title, $revId, ParserOptions $options, $generateHtml, ParserOutput &$output
-	) {
-		if ( $this->isRedirect() ) {
-			$destTitle = $this->getRedirectTarget();
-			if ( $destTitle instanceof Title ) {
-				$output->addLink( $destTitle );
-				if ( $generateHtml ) {
-					$output->setText(
-						Article::getRedirectHeaderHtml( $title->getPageLanguage(), $destTitle )
-					);
-					$output->addModuleStyles( [ 'mediawiki.action.view.redirectPage' ] );
-				}
-			}
-			return;
-		}
-
-		try {
-			$revision = $this->revisionLookup->getRevisionByTitle( $title, $revId );
-			if ( !$revision ) {
-				throw new Exception( 'No revision found' );
-			}
-			$content = $this->mailContentProvider->getContentForRevision( $revision );
-			if ( !$content ) {
-				throw new Exception( 'No content found for single notification' );
-			}
-			$type = $content['meta']['type'];
-			if ( isset( $content['meta']['is_content'] ) && $content['meta']['is_content'] ) {
-				$contentHtml = $this->mailContentProvider->getHtmlFromData(
-					$content,
-					RequestContext::getMain()->getUser(),
-					$content['meta']['sampleData'] ?? []
-				);
-			} else {
-				$contentHtml = $content['meta']['sampleData']['content'] ?? '';
-			}
-			$html = $this->mailContentProvider->wrap(
-				$contentHtml, RequestContext::getMain()->getUser()
-			);
-
-		} catch ( Exception $e ) {
-			$type = 'single';
-			$html = $this->getText();
-		}
-
-		$text = Html::rawElement( 'p', [
-			'style' => 'font-size: 1.2em',
-		], Message::newFromKey( 'notifyme-mail-template-output-header-' . $type )->parse() );
-		if ( $type === 'wrapper' ) {
-			$this->addContentPages( $text );
-		}
-
-		$output->setText( $text . Html::rawElement( 'iframe', [
-			'id' => 'mail-template',
-			'src' => 'about:blank',
-			'width' => '100%',
-			'style' => 'min-height: 800px;',
-			'data-html' => $html,
-		] ) );
-		$output->addModules( [ 'ext.notifyme.mailTemplate' ] );
-	}
-
-	/**
-	 * Add a list of content pages, in case user is on wrapper page
-	 *
-	 * @param string &$text
-	 *
-	 * @return string
-	 */
-	private function addContentPages( string &$text ) {
-		$pages = $this->mailContentProvider->getEmailContentPages();
-		$text .= Html::openElement( 'ul' );
-		foreach ( $pages as $page ) {
-			$text .= Html::rawElement( 'li', [], Html::element( 'a', [
-				'href' => $page->getLocalURL(),
-			], $page->getText() ) );
-		}
-		$text .= Html::closeElement( 'ul' );
-
-		return $text;
-	}
-
 }
--- src/MediaWiki/ContentHandler/MailTemplateHandler.php
+++ src/MediaWiki/ContentHandler/MailTemplateHandler.php
@@ -2,15 +2,30 @@
 
 namespace MediaWiki\Extension\NotifyMe\MediaWiki\ContentHandler;
 
+use Exception;
+use MediaWiki\Content\Content;
+use MediaWiki\Content\Renderer\ContentParseParams;
 use MediaWiki\Content\TextContentHandler;
+use MediaWiki\Context\RequestContext;
+use MediaWiki\Extension\NotifyMe\Channel\Email\MailContentProvider;
 use MediaWiki\Extension\NotifyMe\MediaWiki\Action\EditMailTemplateAction;
 use MediaWiki\Extension\NotifyMe\MediaWiki\Content\MailTemplate;
+use MediaWiki\Html\Html;
+use MediaWiki\Message\Message;
+use MediaWiki\Parser\ParserOutput;
+use MediaWiki\Revision\RevisionLookup;
 
 class MailTemplateHandler extends TextContentHandler {
+
 	/**
 	 * @param string|null $modelId
 	 */
-	public function __construct( $modelId = 'mail_template' ) {
+	public function __construct(
+		string $modelId,
+		private readonly RevisionLookup $revisionLookup,
+		private readonly MailContentProvider $mailContentProvider,
+	) {
+		$modelId = $modelId ?: 'mail_template';
 		parent::__construct( $modelId, [ CONTENT_FORMAT_HTML ] );
 	}
 
@@ -40,6 +55,81 @@ class MailTemplateHandler extends TextContentHandler {
 	 */
 	public function supportsRedirects() {
 		return false;
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	protected function fillParserOutput( Content $content, ContentParseParams $cpoParams, ParserOutput &$output ) {
+		if ( !$content instanceof MailTemplate ) {
+			parent::fillParserOutput( $content, $cpoParams, $output );
+			return;
+		}
+
+		try {
+
+			$revision = $this->revisionLookup->getRevisionByTitle( $cpoParams->getPage(), $cpoParams->getRevId() );
+			if ( !$revision ) {
+				throw new Exception( 'No revision found' );
+			}
+			$mailContent = $this->mailContentProvider->getContentForRevision( $revision );
+			if ( !$mailContent ) {
+				throw new Exception( 'No content found for single notification' );
+			}
+			$type = $mailContent['meta']['type'];
+			if ( isset( $mailContent['meta']['is_content'] ) && $mailContent['meta']['is_content'] ) {
+				$contentHtml = $this->mailContentProvider->getHtmlFromData(
+					$mailContent,
+					RequestContext::getMain()->getUser(),
+					$mailContent['meta']['sampleData'] ?? []
+				);
+			} else {
+				$contentHtml = $mailContent['meta']['sampleData']['content'] ?? '';
+			}
+			$html = $this->mailContentProvider->wrap(
+				$contentHtml, RequestContext::getMain()->getUser()
+			);
+
+		} catch ( Exception $e ) {
+			$type = 'single';
+			$html = $content->getText();
+		}
+
+		$text = Html::rawElement( 'p', [
+			'style' => 'font-size: 1.2em',
+		], Message::newFromKey( 'notifyme-mail-template-output-header-' . $type )->parse() );
+		if ( $type === 'wrapper' ) {
+			$this->addContentPages( $text );
+		}
+
+		$output->setRawText( $text . Html::rawElement( 'iframe', [
+				'id' => 'mail-template',
+				'src' => 'about:blank',
+				'width' => '100%',
+				'style' => 'min-height: 800px;',
+				'data-html' => $html,
+			] ) );
+		$output->addModules( [ 'ext.notifyme.mailTemplate' ] );
+	}
+
+	/**
+	 * Add a list of content pages, in case user is on wrapper page
+	 *
+	 * @param string &$text
+	 *
+	 * @return string
+	 */
+	private function addContentPages( string &$text ) {
+		$pages = $this->mailContentProvider->getEmailContentPages();
+		$text .= Html::openElement( 'ul' );
+		foreach ( $pages as $page ) {
+			$text .= Html::rawElement( 'li', [], Html::element( 'a', [
+				'href' => $page->getLocalURL(),
+			], $page->getText() ) );
+		}
+		$text .= Html::closeElement( 'ul' );
+
+		return $text;
 	}
 
 	/**
