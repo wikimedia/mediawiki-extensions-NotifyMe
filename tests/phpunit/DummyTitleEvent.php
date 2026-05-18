d35a80c36b029dd2c9e41c958b5e76398b3ee609
Hamish <hamish.slater@hallowelt.com>
2026-05-18 21:49:14 +0700

    [tests] Fix serialization of DummyTitleEvent
    ERM20742
    
    [5.1.x]
    
    Change-Id: Ib3d8a18cf04338b3e58d799e9fe2d470d5853cb4

--- tests/phpunit/DummyTitleEvent.php
+++ tests/phpunit/DummyTitleEvent.php
@@ -6,8 +6,25 @@ use MediaWiki\Title\Title;
 use MWStake\MediaWiki\Component\Events\ITitleEvent;
 
 class DummyTitleEvent extends DummyEvent implements ITitleEvent {
+
 	/** @var Title */
-	private $title;
+	protected $title;
+
+	/**
+	 * @return array
+	 */
+	public function __serialize(): array {
+		return parent::__serialize() + [ 'title' => $this->title ];
+	}
+
+	/**
+	 * @param array $data
+	 * @return void
+	 */
+	public function __unserialize( array $data ): void {
+		parent::__unserialize( $data );
+		$this->title = $data['title'];
+	}
 
 	/**
 	 * @return Title
